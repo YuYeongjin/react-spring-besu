f202f6d21cd31821d21681673655b00e3d671811
유 영진 <108460833+YuYeongjin@users.noreply.github.com>
2023-09-03 15:03:17 +0900

    first commit

---
+++ src/test/java/com/example/wordgame/WordGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wordgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
